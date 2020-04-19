cdac28d63e55f224019664c9aad7b61de9a9ed1c
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-04-19 23:16:43 +0100

    Updates

---
+++ precache-manifest.ac429c304aa9ef99df0b49bc4df031a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6bb4e511dda3567a4f6382a5b815b7",
+    "url": "/react-intro-project/index.html"
+  },
+  {
+    "revision": "814df24d20ff61baf566",
+    "url": "/react-intro-project/static/css/main.7635e30e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-intro-project/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro-project/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814df24d20ff61baf566",
+    "url": "/react-intro-project/static/js/main.42fbdc1a.chunk.js"
+  },
+  {
+    "revision": "73ddb58b541d9d494c2d",
+    "url": "/react-intro-project/static/js/runtime-main.c6910ad3.js"
+  }
+]);
