edfbdf00437227aebcacbae7fbe84a67934d4a9d
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-04-20 07:08:05 +0100

    fix: robohash id

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ import './card-list.styles.css';
 export const CardList = props => (
   <div className='card-list'>
     {props.monsters.map(monster => (
-      <Card key={monster.id} monster={monster}/>
+      <Card key={monster.id} monster={monster} />
     ))}
   </div>
 )
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,9 @@ import './card.styles.css';
 
 export const Card = props => (
   <div className='card-container'>
-    <img src={`https://robohash.org/{props.monster.id}?set=set2&size=180x180`} alt='monster' />
+    <img
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt='monster'
+    />
     <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
